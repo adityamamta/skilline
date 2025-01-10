51c4f9b1a5559ad90a0205fc29dbaf91f08bc508
Aditya <adityamamta4gmail.com>
2025-01-10 20:24:27 +0530

    scroll-reveal added

--- myscript.js
+++ myscript.js
@@ -9,10 +9,7 @@ navToggle.addEventListener("click", () => {
 })
 
 
-window.addEventListener('load', () => {
-    const loader = document.querySelector('.loader');
-    loader.classList.add('hide-loader');
-});
+
 
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1, // Show 1 slide at a time
@@ -22,3 +19,33 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.prev-btn',
     },
 });
+
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('hide-loader');
+
+    ScrollReveal().reveal('.slide-up', {
+    distance: '50px', // How far the element moves
+    duration: 800, // Animation duration in milliseconds
+    easing: 'ease', // Easing function
+    origin: 'bottom', // Animation start position
+    // reset: true // Animations reset when re-entering the viewport
+    });
+    ScrollReveal().reveal('.delay-400', {
+        delay: 400,
+    });
+    ScrollReveal().reveal('.delay-600', {
+        delay: 600,
+    });
+    ScrollReveal().reveal('.delay-800', {
+        delay: 800,
+    });
+    ScrollReveal().reveal('.fade-in', {
+        duration: 1000,
+        easing: 'ease',
+        delay: 400,
+    })
+
+});
+
+
